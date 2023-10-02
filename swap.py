85346b3cd0b8d9f85e55d5dfc44a68411b96ceba
Prateek Sinha <111572443+CDT-PRATEEK@users.noreply.github.com>
2023-10-02 20:35:58 +0530

    Optimized Swap.py
    I have optimized swap.py using tuple unpacking and error handling.

--- swap.py
+++ swap.py
@@ -1,19 +1,31 @@
-# Function to swap two numbers
-def swap_numbers(a, b):
-    temp = a
-    a = b
-    b = temp
-    return a, b
 
-# Input two numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+def swap_numbers(first_number, second_number):
+    """
+    Swaps the values of two numbers.
 
-# Displaying numbers before swapping
+    Args:
+        first_number (float): The first number.
+        second_number (float): The second number.
+
+    Returns:
+        Tuple[float, float]: A tuple containing the swapped numbers.
+    """
+    first_number, second_number = second_number, first_number
+    return first_number, second_number
+
+try:
+   
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+    exit(1)  
+
+
 print(f"Before swapping: num1 = {num1}, num2 = {num2}")
 
-# Swapping numbers
+
 num1, num2 = swap_numbers(num1, num2)
 
-# Displaying numbers after swapping
+
 print(f"After swapping: num1 = {num1}, num2 = {num2}")
