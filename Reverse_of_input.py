5fac7a9df9ed58a202f76f01001ba4d7cc08bf76
R0Y15 <bishalroy15jan@gmail.com>
2023-10-02 23:41:36 +0530

    a code that can reverse any given input

---
+++ Reverse_of_input.py
@@ -0,0 +1,15 @@
+# A code to reverse the input 
+inp = input("Enter your input: ")
+
+if inp.isnumeric():
+    number = int(inp)
+    reversed_number = 0
+    while number > 0:
+        remainder = number % 10
+        reversed_number = (reversed_number * 10) + remainder
+        number = number // 10
+    print("Reversed number:", reversed_number)       # input: 12345 ==> output: 54321
+
+else:
+    reversed_string = inp[::-1]
+    print("Reversed string:", reversed_string)      # input: "hello" ==> output: "olleh"
