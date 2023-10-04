89fc642281a5dcb453ae176e08ae49497bf47146
vermasumi <sverma21-ece@bvucoep.edu.in>
2023-10-05 02:02:17 +0530

    python program to find HCF or GCD

---
+++ HCForGCD.py
@@ -0,0 +1,23 @@
+#Python Program to find HCF or GCD
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def find_hcf(a, b):
+    # HCF is the same as GCD
+    return find_gcd(a, b)
+
+def main():
+    # Example usage
+    num1 = 48
+    num2 = 18
+
+    gcd = find_gcd(num1, num2)
+    hcf = find_hcf(num1, num2)
+
+    print(f"The GCD of {num1} and {num2} is: {gcd}")
+    print(f"The HCF of {num1} and {num2} is: {hcf}")
+
+if __name__ == "__main__":
+    main()
