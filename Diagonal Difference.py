53236671e15771fb4f175fc039f8232e5ae97349
GOPAL GORAI <142316387+gopalgorai001@users.noreply.github.com>
2023-10-07 08:47:41 +0530

    Create Diagonal Difference.py

---
+++ Diagonal Difference.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    temp = 0
+    emp = 0
+    for i in range(0,len(arr)):
+        temp = temp + arr[i][i]
+    
+    for j in range(0,len(arr)):
+        emp = emp + arr[j][len(arr)-1-j]
+    
+    return abs(temp - emp)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
