6981d3cb40aaad72bd93f05b12c3437c9f2b1a17
Onkar310 <105639953+Onkar310@users.noreply.github.com>
2023-10-23 01:57:45 +0530

    Revert "Add files via upload"

--- Meme generator/index.js
+++
@@ -1,7 +0,0 @@
-async function fetchData(params) {
-  res = await fetch("https://meme-api.com/gimme/1");
-  res = await res.json();
-  var val = res.memes[0].url;
-  var image = document.querySelector("#img");
-  image.src = val;
-}
