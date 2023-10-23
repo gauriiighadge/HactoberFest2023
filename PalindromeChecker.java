4d21cd9478f7fcab5668aab381de483a7f4bafe1
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-10-23 23:00:37 +0530

    Create PalindromeChecker.java
    In this program, you can input either a string or an integer, and it will determine whether the input is a palindrome. The Scanner class is used to read input from the user. Depending on the type of input (string or integer), it calls the corresponding isPalindrome function to check for a palindrome.
    
    To test this program, compile and run it. Enter a string or an integer when prompted, and it will tell you if it's a palindrome.

---
+++ PalindromeChecker.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Palindrome Checker");
+
+        System.out.print("Enter a string or an integer: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll(" ", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+}
