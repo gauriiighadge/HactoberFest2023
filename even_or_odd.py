02a9669ae31c46050082917bace9be5c193f03bd
vidya71 <143339493+vidya71@users.noreply.github.com>
2023-10-27 18:36:46 +0530

    modified the program of even or odd to check all type of numbers

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,8 +1,16 @@
-# Get user input for a number
-number = int(input("Enter a number: "))
+def check_even_odd(num):
+    num1 = str(num)
+    num2 = num1[len(num1)-1]
+    num3 = int(num2)
+    if num3 % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
 
-# Check if the number is even or odd
-if number % 2 == 0:
-    print(f"{number} is even.")
-else:
-    print(f"{number} is odd.")
+while True:
+    try:
+        num = input("Enter a decimal number: ")
+        check_even_odd(num)
+        break
+    except ValueError:
+        print("Invalid input. Please enter a decimal number.")
